d7e01cdb5ed8ed8e9c2a44afa101c0a2d3a767e3
chtah <chawakorn_ha@hotmail.com>
2023-11-29 20:13:36 +0700

    change admin

--- src/const.ts
+++ src/const.ts
@@ -6,4 +6,4 @@ if (!ENV_JWI_SECRET)
 
 export const JWT_SECRET = ENV_JWI_SECRET;
 
-export const adminID = "24d17988-c92c-447f-aa2b-48969187dae2";
+export const adminID = "8c6e2dc4-cbd4-49ed-bf5d-b383862bac31";
